15d79cdc2758b7de4e2d7a4b463f74f4ffafe743
lovzan <lovzan@gmail.com>
2017-10-26 16:05:00 +0200

    Update MySQLBackup.sh

--- MySQLBackup.sh
+++ MySQLBackup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # MySQL Backup
 # Backup Filename
-Filename="backup_$(date +%Y%m%d_%H%M%S).sql.zip"
+FILENAME="backup_$(date +%Y%m%d_%H%M%S).sql.zip"
 # Backup & Compress
-mysqldump -uMYSQL_USER -pMYSQL_PASSWORD MYSQL_DATABASE_NAME | zip -P ZIP_PASSWORD > /home/backup/mysql/$Filename
+mysqldump -uMYSQL_USER -pMYSQL_PASSWORD MYSQL_DATABASE_NAME | zip -P ZIP_PASSWORD > /home/backup/mysql/$FILENAME
 # Keep only the last 30 days backup"
 find /home/backup/mysql/* -mtime +30 -exec rm {} \;
 # FTP Transfert
-wput -u /home/backup/mysql/$Filename ftp://FTP_USER:FTP_PASSWORD@FTP_HOST
+wput -u /home/backup/mysql/$FILENAME ftp://FTP_USER:FTP_PASSWORD@FTP_HOST
 # END
